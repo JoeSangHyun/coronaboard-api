b119858069110032dd3cf6f8d89e15f63cbb804c
JoeSangHyun <tiamet133@naver.com>
2022-07-21 12:10:46 +0900

    첫번째 커밋

---
+++ controller/key-value.controller.js
@@ -0,0 +1,52 @@
+const { GlobalStat, KeyValue } = require('../database'); // GlobalStat 객체 가져오기
+const { wrapWithErrorHandler } = require('../util');
+
+// 데이터 조회
+async function get(req,res) {
+   const { key } = req.params;
+   if(!key) {
+    res.status(400).json({ error: 'key is required'});
+    return;
+   }
+
+   const result = await KeyValue.findOne({
+    where: { key },
+   });
+   res.status(200).json({ result });
+}
+
+// 데이터 삽입 또는 업데이트
+async function insertOrUpdate(req,res) {
+    const { key, value } = req.body;
+    if(!key || !value )  {
+        res.status(400).json({error: 'key or value are required '});
+        return;
+    }
+    
+    await KeyValue.upsert({ key, value});
+
+    res.status(200).json({ result: 'success'});
+}
+
+// 데이터 삭제
+async function remove(req, res) {
+    const { key } = req.body;
+    if(!key) {
+        res.status(400).json({ error: 'key required'});
+        return;
+    }
+
+    await KeyValue.destroy({
+        where: {
+            key,
+        },
+    });
+
+    res.status(200).json({ result: 'success' });
+}
+
+module.exports = wrapWithErrorHandler({
+    get,
+    insertOrUpdate,
+    remove,
+});
